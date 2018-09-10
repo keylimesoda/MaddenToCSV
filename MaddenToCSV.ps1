aba4702c6d942c047f97c26a5edf43f30acb0cf0
keylimesoda <ric.lewis@gmail.com>
2018-09-09 18:11:43 -0700

    Updating script notes

--- MaddenToCSV.ps1
+++ MaddenToCSV.ps1
@@ -3,10 +3,18 @@
         Function to export League Info, Schedules and Rosters to CSV files on-disk
         
     .DIRECTIONS
-        The script will run indefinitely.  Close the window when you're done.
-
+    	Before 1st run, you must enable powershell scripts on your computer
+		Open a powershell window as administrator
+		Type "Set-ExecutionPolicy Bypass"
+	Run the MaddenToCSV.ps1 script
+	Enter the server address shown on the PowerShell window into your Madden Companion App, and Export
+	.CSV files will be saved onto your PC
+	The script will run indefinitely.  Close the window when you're done.
+	
     .NOTES
-        Does not yet handle stats
+        Team names are not included in the stats and roster files. They must be mapped from the table in leagueInfo.csv using the TeamID.
+	The app exports 8 stat tables for each week: (schedules, defense, kicking, punting, passing, receiving, rushing, teamstats)
+
 #>
 
 Add-Type -AssemblyName System.IO
